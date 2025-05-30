5eaaed6b425a0c64ceedddaee41152bb0ebc0bc7
Ned Batchelder <ned@nedbatchelder.com>
2025-05-30 13:17:19 -0400

    Add more logging to run_cmd

--- ick/sh.py
+++ ick/sh.py
@@ -15,7 +15,16 @@ LOG = getLogger(__name__)
 def run_cmd(cmd: list[Union[str, Path]], check: bool = True, cwd: Optional[Union[str, Path]] = None, **kwargs) -> Tuple[str, int]:
     cwd = cwd or os.getcwd()
     LOG.info("Run %s in %s", cmd, cwd)
-    proc = subprocess.run(cmd, encoding="utf-8", capture_output=True, check=check, cwd=cwd, **kwargs)
+    try:
+        proc = subprocess.run(cmd, encoding="utf-8", capture_output=True, check=check, cwd=cwd, **kwargs)
+    except subprocess.CalledProcessError as e:
+        LOG.warning("Ran %s -> %s", cmd, e.returncode)
+        if e.stdout:
+            LOG.warning("Stdout:\n%s", e.stdout)
+        if e.stderr:
+            LOG.warning("Stderr:\n%s", e.stderr)
+        raise
     LOG.debug("Ran %s -> %s", cmd, proc.returncode)
-    LOG.debug("Stdout: %s", proc.stdout)
+    LOG.debug("Stdout:\n%s", proc.stdout)
+    LOG.debug("Stderr:\n%s", proc.stderr)
     return proc.stdout, proc.returncode
