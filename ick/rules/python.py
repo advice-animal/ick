52202420aaf42e740f1d1f9740239c0b3f772976
Ned Batchelder <ned@nedbatchelder.com>
2026-01-09 15:02:11 -0500

    ICK_COVERAGE_PY=1 runs tests under coverage

--- ick/rules/python.py
+++ ick/rules/python.py
@@ -14,18 +14,39 @@ from ..venv import PythonEnv
 class Rule(BaseRule):
     def __init__(self, rule_config: RuleConfig) -> None:
         super().__init__(rule_config)
+
+        coverage = bool(int(os.environ.get("ICK_COVERAGE_PY", "0")))
+
         # TODO validate path / rule.name ".py" exists
         assert rule_config.qualname != ""
         venv_key = rule_config.qualname
         venv_path = Path(platformdirs.user_cache_dir("ick", "advice-animal"), "envs", venv_key)
-        self.venv = PythonEnv(venv_path, self.rule_config.deps)
+        deps = self.rule_config.deps
+        if coverage:
+            deps = [*(deps or []), "coverage"]
+        self.venv = PythonEnv(venv_path, deps)
 
         self.command_parts = [self.venv.bin("python")]
 
         if rule_config.data:
             self.command_parts.extend(["-c", textwrap.dedent(rule_config.data)])
         else:
-            py_script = rule_config.script_path.with_suffix(".py")  # type: ignore[union-attr] # FIX ME
+            if coverage:
+                coveragerc = "/tmp/ick_cov_config.ini"
+                Path(coveragerc).write_text(textwrap.dedent(f"""\
+                    [run]
+                    branch = True
+                    data_file = {os.getcwd()}/.coverage
+                    parallel = True
+                    source = {self.rule_config.script_path.parent}
+
+                    [report]
+                    partial_also =
+                        if __name__ == .__main__.:
+
+                    """))
+                self.command_parts += ["-m", "coverage", "run", "--rcfile", coveragerc]
+            py_script = self.rule_config.script_path.with_suffix(".py")  # type: ignore[union-attr] # FIX ME
             if not py_script.exists():
                 self.runnable = False
                 self.status = f"Couldn't find implementation {py_script}"
